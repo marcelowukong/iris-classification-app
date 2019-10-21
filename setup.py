7599c0973dd2d8195800093a9add1aeae1505631
marcelowukong <56727193+marcelowukong@users.noreply.github.com>
2019-10-20 21:20:49 -0300

    Test

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is perhaps the best known database to be found in the pattern recognition literature. Fisher's paper is a classic in the field and is referenced frequently to this day.',
+    author='Marcelo Santos',
+    license='',
+)
